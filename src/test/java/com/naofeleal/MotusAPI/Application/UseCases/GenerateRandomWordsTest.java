0cd9b00e65cfd9802611c916e822e77de235182a
Naofel-eal <naofel.eal@gmail.com>
2023-11-12 23:25:31 +0100

    fix spy test in generateRandomWordsTest

--- src/test/java/com/naofeleal/MotusAPI/Application/UseCases/GenerateRandomWordsTest.java
+++ src/test/java/com/naofeleal/MotusAPI/Application/UseCases/GenerateRandomWordsTest.java
@@ -1,19 +1,31 @@
 package com.naofeleal.MotusAPI.Application.UseCases;
 
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
+import com.naofeleal.MotusAPI.Application.Mappers.OutputWordMapper;
+import com.naofeleal.MotusAPI.Application.Mappers.OutputWordMapperImpl;
 import com.naofeleal.MotusAPI.Application.Presenters.GenerateRandomWordsPresenter;
 
+@SpringBootTest(classes = OutputWordMapperImpl.class)
 public class GenerateRandomWordsTest {
     private GenerateRandomWords _generateRandomWords;
+    private GenerateRandomWordsPresenter _presenter;
+
+    @BeforeEach()
+    public void setUp() {
+        OutputWordMapper outputWordMapper = Mappers.getMapper(OutputWordMapper.class);
+        _presenter = new GenerateRandomWordsPresenter(outputWordMapper);
+    }
 
     @Test()
     public void it_should_call_repository() {
         // Arrange
         SpyWordRepository spyWordRepository = new SpyWordRepository();
-        GenerateRandomWordsPresenter presenter = new GenerateRandomWordsPresenter();
-        _generateRandomWords = new GenerateRandomWords(spyWordRepository, presenter);
+        _generateRandomWords = new GenerateRandomWords(spyWordRepository, _presenter);
         
         // Act
         _generateRandomWords.execute(5);
